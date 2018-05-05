85ba4c00027908d5ef8673eaf587a5ca35436831
tyzesc <tyzescgm@gmail.com>
2018-05-05 18:15:38 +0800

    180505 edit
    Description listen multiple port with config.json. and fix index2.js wrong code

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM node:latest
 COPY . /tyze-mcbaobao-proxy
 WORKDIR /tyze-mcbaobao-proxy
-EXPOSE 8081
 CMD npm install && npm start
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var Thin = require('./lib/thin.js');
-var gunzip = require('zlib').createGunzip();
-var request = require('request');
-
-var proxy = new Thin;
+var fs = require('fs');
 
 function dateString(obj) {
     let yyyy = obj.toLocaleDateString().slice(0, 4)
@@ -45,95 +42,108 @@ function coupon(id) {
     }
     return c;
 }
-//var available = "11,12,14,15,16,17,18,19,20,21,23,24,25,26,32,33,34,35,36,37,38,39,42,43,44,45,46,47,48,49,50,51,52,53,56,57,58,61,65,66,69,70,71,72,77,78,81,83,89,90,93,94,95,96,97,98,99,100,106,107,108,109,110,111,112,113,114,115,116,119,120,123,124,126,127,128,129,130,131,132,133,134,136,138,139,140,141,142,143,144,201".split(',');
 
-var coupons = [];
-var available = "66,65,53,48,44,43,42,39,37,24,21,20,19,17,15".split(',');
+var Server = function(port, str) {
+    var self = this;
+    self.thin = new Thin;
+    self.proxy = new Thin();
+    self.str = str;
+    self.port = port;
 
-function updateCoupons(coupons, list) {
-    coupons.splice(0, 10000);
-    list.map((elem, i) => {
-        let obj = coupon(elem);
-        obj.coupon_id = i;
-        //console.log(obj.object_info.image.url);
-        coupons.push(obj);
-        //console.log("[*] 載入優惠券：" + i + " " + obj.object_info.title);
-    });
-    console.log("[*] 刷新優惠券");
-}
+    self.coupons = [];
 
-updateCoupons(coupons, available);
-
-let a = 0;
-proxy.use(function(req, res, next) {
-    if (['/coupon/get_detail', '/coupon/redeem', '/coupon/get_list'].indexOf(req.url) == -1)
-        next();
-    if (req.url === '/coupon/get_detail') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString(); // convert Buffer to string
+    self.updateCoupons = function() {
+        list = self.str.split(',');
+        self.coupons.splice(0, 10000);
+        list.map((elem, i) => {
+            let obj = coupon(elem);
+            obj.coupon_id = i;
+            self.coupons.push(obj);
         });
-        req.on('end', () => {
-            let json = JSON.parse(body);
-            let id = json.coupon_id;
-            console.log("[*] 編號：" + id + " " + coupons[id].object_info.title + " 被開啟。");
-            let ret = {
+        console.log("[*] 刷新" + self.port + "優惠券");
+    }
+
+    self.updateCoupons();
+    
+    self.proxy.use((req, res, next) => {
+        if (['/coupon/get_detail', '/coupon/redeem', '/coupon/get_list'].indexOf(req.url) == -1)
+            next();
+        if (req.url === '/coupon/get_detail') {
+            let body = '';
+            req.on('data', chunk => {
+                body += chunk.toString(); // convert Buffer to string
+            });
+            req.on('end', () => {
+                let json = JSON.parse(body);
+                let id = json.coupon_id;
+                console.log("[*] 編號：" + id + " " + self.coupons[id].object_info.title + " 被開啟。");
+                let ret = {
+                    "rc": 1,
+                    "rm": "成功",
+                    "results": {
+                        "coupon": self.coupons[id],
+                        "current_datetime": datetime
+                    }
+                }
+                return res.end(JSON.stringify(ret));
+            });
+        }
+        if (req.url === '/coupon/redeem') {
+            let body = '';
+            req.on('data', chunk => {
+                body += chunk.toString();
+            });
+            req.on('end', () => {
+                let json = JSON.parse(body);
+                let id = json.coupon_id;
+                console.log("[*] 編號：" + id + " " + self.coupons[id].object_info.title + " 被兌換。");
+                self.coupons[id].status = 2;
+                setTimeout(function() {
+                    self.coupons[id].status = 1;
+                }, 1000 * 60);
+
+                let ret = {
+                    "rc": 1,
+                    "rm": "成功",
+                    "results": {
+                        "coupon": self.coupons[id],
+                        "redeem_datetime": redeemtime
+                    },
+                    "current_datetime": datetime
+                }
+
+                return res.end(JSON.stringify(ret));
+            });
+        }
+        if (req.url === '/coupon/get_list') {
+            let json = {
                 "rc": 1,
                 "rm": "成功",
                 "results": {
-                    "coupon": coupons[id],
+                    "coupons": self.coupons,
                     "current_datetime": datetime
                 }
             }
-            return res.end(JSON.stringify(ret));
-        });
-    }
-    if (req.url === '/coupon/redeem') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            let json = JSON.parse(body);
-            let id = json.coupon_id;
-            console.log("[*] 編號：" + id + " " + coupons[id].object_info.title + " 被兌換。");
-            coupons[id].status = 2;
-            setTimeout(function(){
-                coupons[id].status = 1;
-            }, 1000 * 60);
-
-            let ret = {
-                "rc": 1,
-                "rm": "成功",
-                "results": {
-                    "coupon": coupons[id],
-                    "redeem_datetime": redeemtime
-                },
-                "current_datetime": datetime
-            }
-
-            return res.end(JSON.stringify(ret));
-        });
-    }
-    if (req.url === '/coupon/get_list') {
-        let json = {
-            "rc": 1,
-            "rm": "成功",
-            "results": {
-                "coupons": coupons,
-                "current_datetime": datetime
-            }
+            return res.end(JSON.stringify(json));
         }
-        return res.end(JSON.stringify(json));
+    });
+
+    self.proxy.listen(port, '0.0.0.0', function(err) {});
+    console.log("[*] Server is on " + '0.0.0.0' + ":" + port);
+    return self;
+}
+
+var rout = {};
+
+fs.readFile('config.json', 'utf8', (err, data) => {
+    if (err) {
+        console.log("config.json open failed.");
+        process.exit(1);
     }
-
+    let json = JSON.parse(data);
+    let routers = json.routers;
+    for (let port in routers) {
+        let str = routers[port];
+        rout[port] = new Server(port, str);
+    }
 });
-
-var port = process.env.PORT || 8081;
-var ip = "0.0.0.0" || process.env.LOCALHOST || "localhost";
-proxy.listen(port, ip, function(err) {
-    // .. error handling code ..
-});
-
-console.log("APP is on " + ip + " " + port);
-console.log('[*] 麥當勞真的爆爆～～');
